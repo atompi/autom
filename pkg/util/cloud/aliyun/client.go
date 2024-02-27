62129f2c7c7b3c921fe3795be929c51cb8fd3909
atompi <coder.atompi@gmail.com>
2024-02-27 23:12:32 +0800

    fix: aliyun util

--- pkg/util/cloud/aliyun/client.go
+++ pkg/util/cloud/aliyun/client.go
@@ -2,16 +2,14 @@ package aliyun
 
 import (
 	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
-	tag20180828 "github.com/alibabacloud-go/tag-20180828/v2/client"
 	"github.com/alibabacloud-go/tea/tea"
 )
 
-func CreateTag20180828Client(accessKeyId, accessKeySecret, regionId, endpoint *string) (*tag20180828.Client, error) {
+func CreateClientConfig(accessKeyId, accessKeySecret, regionId, endpoint *string) *openapi.Config {
 	config := &openapi.Config{}
 	config.AccessKeyId = accessKeyId
 	config.AccessKeySecret = accessKeySecret
 	config.RegionId = regionId
 	config.Endpoint = tea.String(*endpoint)
-
-	return tag20180828.NewClient(config)
+	return config
 }
